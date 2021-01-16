1af95befe7e098ccc49070c9528ac0e9a4da56c1
Arturo Suárez <masuareb@hotmail.com>
2021-01-16 16:15:29 -0600

    Spring Boot Application - Initial Commit

---
+++ src/test/java/dev/masuareb/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.masuareb.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
